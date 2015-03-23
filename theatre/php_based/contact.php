93aafd070a04086fb6942457fbaa625deeafc313
titouanfreville <titouanfreville@hotmail.fr>
2015-03-23 13:06:29 +0100

    Update on theatre
    moved files

--- theatre/php_based/contact.php
+++ theatre/php_based/contact.php
@@ -1,54 +1,70 @@
 <?php
-
-if($_POST["Envoyer"]) {
-    $sendern=$_POST["sendern"];
-    $senderp=$_POST["senderp"];
-    $senderEmail=$_POST["senderEmail"];
-    $message= $_POST["message"];
-    $message = wordwrap($message, 70, "\r\n");
-
-    $headers = 'From: '. $senderEmail . "\r\n" .
-            'Reply-To: '. $senderEmail . "\r\n" .
-            'X-Mailer: PHP/' . phpversion();
-
-    $sender=$sendern.$senderp;
-
-    $mailBody="Name: $sender\nEmail: $senderEmail\r\n$message";
-
-    if (mail('titouanfreville@hotmail.fr,contact@festival-saint-valery.fr', 'subject', $message, $headers)) {
-      $thankYou="Thank you! Your message has been sent.";
-      echo "<script type='text/javascript'>alert('$thankYou');</script>";
-    }
-}
-
+  include 'utilities/utilities.php';
+  full_header(contact);
 ?>
-<!DOCTYPE html>
-
-<html>
-  <head>
-      <meta charset="utf-8">
-      <title>Formulaire de contact Theatre en Festival</title>
-      <link href="./stylesheets/style.css" rel="stylesheet" type="text/css" title="style" />
-  </head>
-
-  <body>
-    <form name="f" method="post" action="contact.php">
-      <label>
-        <span>Votre nom *</span><input type="text" name="sendern" value=""/>
-      </label>
-      <label>
-       <span>Votre prénom *</span><input type="text" name="senderp" value=""/>
-      </label>
-      <label>
-       <span>Votre e-mail *</span><input type="text" name="senderEmail" value=""/>
-      </label>
-      <label>
-        <span>Votre message *</span><textarea name="message" rows="7" cols="35" value=""></textarea>
-      </label>
-      <span> * champs obligatoires </span>
-      <input type="submit" name="Envoyer">
-    </form>
-
-  </body>
-
-</html>
+      <header>
+        <span><p>nous </p> contacter</span>
+      </header>
+      <div class='contact'>
+        <h1>Pour nous joindre :</h1>
+        <ul>
+          <li>
+            <div class='way'>via ce site :</div>
+            <div class='solution'>
+              cliquez sur
+              <a class='link' href='./formcontact.html'>ce lien</a>
+              pour ouvrir le formulaire de contact.
+            </div>
+          </li>
+          <li>
+            <div class='way'>par e-mail :</div>
+            <div class='solution'>
+              <a class='link' href='mailto:contact@festival-saint-valery.fr'>contact@festival-saint-valery.fr</a>
+            </div>
+          </li>
+          <li>
+            <div class='way'>par téléphone :</div>
+            <div class='solution'>03 22 60 64 34</div>
+          </li>
+          <li>
+            <div class='way'>par courrier :</div>
+            <div class='solution'>Association Théâtre en Festival<br />Mairie<br />19 Place Saint Martin<br />80230 Saint Valery sur Somme</div>
+          </li>
+        </ul>
+        <h1>Si vous souhaitez joindre directement l'un des membres du bureau de l'association :</h1>
+        <ul>
+          <li>
+            <div class='title'>Présidente</div>
+            <div class='name'>Jeannine PICHON</div>
+            <a class='link' href='mailto:pichon.jeannine@orange.fr'>pichon.jeannine@orange.fr</a>
+          </li>
+          <li>
+            <div class='title'>Vice Présidente</div>
+            <div class='name'>Marie-Agnès LANDIÉ</div>
+            <a class='link' href='mailto:mireaux.landie@cegetel.net'>mireaux.landie@cegetel.net</a>
+          </li>
+          <li>
+            <div class='title'>Secrétaire</div>
+            <div class='name'>Marie-Colette MORÉNAS</div>
+            <a class='link' href='mailto:mcmorenas@free.fr'>mcmorenas@free.fr</a>
+          </li>
+          <li>
+            <div class='title'>Vice Secrétaire</div>
+            <div class='name'>Maryse MONTASSINE</div>
+            <a class='link' href='mailto:maryse.montassine@orange.fr'>maryse.montassine@orange.fr</a>
+          </li>
+          <li>
+            <div class='title'>Trésorier</div>
+            <div class='name'>Pierre LAMIDEL</div>
+            <a class='link' href='mailto:pierre.lamidel80@orange.fr'>pierre.lamidel80@orange.fr</a>
+          </li>
+          <li>
+            <div class='title'>Vice Trésorier</div>
+            <div class='name'>Dominique VERET</div>
+            <a class='link' href='mailto:veret.dominique@orange.fr'>veret.dominique@orange.fr</a>
+          </li>
+        </ul>
+      </div>
+<?php
+  eof();
+?>
---
+++ theatre/php_based/utilities/sponsoring.php
@@ -0,0 +1,62 @@
+<?php
+
+  // Print the sponsor slider
+  function sp_slide()
+  {
+    echo "<!-- Slider partenaire -->\n";
+    echo "<section>\n";
+    echo "<div class='pacommand' id='pa_play'></div>\n";
+    echo "<div class='pacommand' id='pa_pause'></div>\n";
+    echo "<div class='slideshow'>\n";
+    echo "<a class='play_commands pause' href='#pa_pause' title='pause'>pause</a>\n";
+    echo "<a class='play_commands play' href='#pa_play' title='play'>play</a>\n";
+    echo "<div class='container'>\n";
+    echo "<div class='slider animate'>\n";
+    echo "<figure><img class='anim' src='images/stvalery_defile.png'></figure><figure>\n";
+    echo "<img class='anim' src='images/comdecom_defile.png'>\n";
+    echo "</figure><figure>\n";
+    echo "<img class='anim' src='images/somme_defile.png'>\n";
+    echo "</figure><figure>\n";
+    echo "<img class='anim' src='images/region_defile.png'>\n";
+    echo "</figure><figure>\n";
+    echo "<img class='anim' src='images/picardiefr3_defile.png'>\n";
+    echo "</figure><figure>\n";
+    echo "<img class='anim' src='images/intermarche_defile.png'>\n";
+    echo "</figure><figure>\n";
+    echo "<img class='anim' src='images/garagebarbier_defile.png'>\n";
+    echo "</figure><figure>\n";
+    echo "<img class='anim' src='images/delahaye_defile.png'>\n";
+    echo "</figure><figure>\n";
+    echo "<img class='anim' src='images/france_bleu_defile.png'>\n";
+    echo "</figure><figure>\n";
+    echo "<img class='anim' src='images/picardia_defile.png'>\n";
+    echo "</figure><figure>\n";
+    echo "<img class='anim' src='images/nautic_defile.png'>\n";
+    echo "</figure><figure>\n";
+    echo "<img class='anim' src='images/hyperu_defile.png'>\n";
+    echo "</figure><figure>\n";
+    echo "<img class='anim' src='images/proxinews_defile.png'>\n";
+    echo "</figure><figure>\n";
+    echo "<img class='anim' src='images/courrierpicard_defile.png'>";
+    echo "</figure><figure>\n";
+    echo "<img class='anim' src='images/picardieweb_defile.png'>\n";
+    echo "</figure><figure>\n";
+    echo "<img class='anim' src='images/journaldabbeville_defile.png'>\n";
+    echo "</figure><figure>\n";
+    echo "<img class='anim' src='images/eclaireur_defile.png'>\n";
+    echo "</figure><figure>\n";
+    echo "<img class='anim' src='images/ecbs_defile.png'>\n";
+    echo "</figure><figure>\n";
+    echo "<img class='anim' src='images/evasion_defile.png'>\n";
+    echo "</figure><figure>\n";
+    echo "<img class='anim' src='images/irem_defile.png'>\n";
+    echo "</figure><figure>\n";
+    echo "<img class='anim' src='images/bateau_baie_desomme_defile.png'>\n";
+    echo "</figure><figure>\n";
+    echo "<img class='anim' src='images/nora_defile.png'>\n";
+    echo "</figure></div>\n";
+    echo "</div>\n";
+    echo "</div>\n";
+    echo "</section>\n";
+    echo "<!-- End slider -->\n";
+  }
