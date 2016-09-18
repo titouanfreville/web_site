5e4764022c768f60d84ccdd87fa328c13e9cc6e4
titouanfreville <titouanfreville@hotmail.fr>
2016-09-18 21:05:23 +0200

    Added some style

--- cactusespacesverts/css/compilation.sh
+++ cactusespacesverts/css/compilation.sh
@@ -8,6 +8,6 @@ echo "Mode de Compilation : Debug(d) ou Production(P)"
 read mode
 
 case "$mode" in
-  d) compass watch --sass-dir $src --force -s expand $dest;;
-  *) compass watch --sass-dir $src --force -s compressed --no-line-comments $dest;;
+  d) compass watch --sass-dir $dest --force -s expand $src;;
+  *) compass watch --sass-dir $dest --force -s compressed --no-line-comments $src;;
 esac;
---
+++ cactusespacesverts/css/config.rb
@@ -0,0 +1,14 @@
+require 'compass/import-once/activate'
+# Require any additional compass plugins here.
+
+# Set this to the root of your project when deployed:
+http_path = "/"
+css_dir = "/"
+asset_cache_buster = :none
+cache = false
+# You can select your preferred output style here (can be overridden via the command line):
+output_style = :expanded
+
+
+# To disable debugging comments that display the original location of your selectors. Uncomment:
+# line_comments = false
